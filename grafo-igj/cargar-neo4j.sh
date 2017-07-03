95be4b07cca73c9088917a354306428cbd29823b
Martín Szyszlican <martinsz@gmail.com>
2017-07-03 12:13:40 -0300

    Agrego labels a la importacion en neo4j (colorcitos ftw!)

--- grafo-igj/cargar-neo4j.sh
+++ grafo-igj/cargar-neo4j.sh
@@ -1,2 +1,2 @@
-./neo4j-admin import --database=igj.db --relationships="../import/relaciones-headers.csv,../import/relaciones_limpio.csv"  --nodes="../import/nodos_empresas-headers.csv,../import/nodos_empresas.csv"  --nodes="../import/nodos_personas-headers.csv,../import/nodos_personas.csv"
+./neo4j-admin import --database=igj.db --relationships="../import/relaciones-headers.csv,../import/relaciones_limpio.csv"  --nodes:Entidad "../import/nodos_empresas-headers.csv,../import/nodos_empresas.csv"  --nodes:Autoridad "../import/nodos_personas-headers.csv,../import/nodos_personas.csv"
 
